f41fa06e93fa04e48e47496d70a670619ad7d245
meriem0319 <meriem0319@gmail.com>
2022-10-10 15:00:21 -0400

    role util done

---
+++ utils/role.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+const db = require("../db/connections");
+
+const showRole = function () {
+  const sql = `SELECT * FROM roles`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    console.log("\nRoles Table");
+    console.table(rows);
+  });
+};
+
+const addRole = function () {
+  inquirer
+    .createPromptModule([
+      {
+        type: "text",
+        name: "title",
+        message: "What is the role title being added?",
+      },
+      {
+        type: "number",
+        name: "salary",
+        message: "What is the salary for the role being added?",
+      },
+      {
+        type: "text",
+        name: "department",
+        message: "Which department is this role being added to?",
+      },
+    ])
+    .then((answers) => {
+      const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+      const params = [answers.title, answers.salary, answers.department];
+      db.query(sql, params, (err, result) => {
+        if (err) {
+          console.log(err.message);
+          return;
+        }
+        console.log("Added Successfully!");
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { showRole, addRole };
