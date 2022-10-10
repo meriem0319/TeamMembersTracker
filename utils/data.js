db83122de098c8640c1186781a18f073dceecc85
meriem0319 <meriem0319@gmail.com>
2022-10-10 14:31:44 -0400

    data util is done

---
+++ utils/data.js
@@ -0,0 +1,28 @@
+const db = require("../db/connections");
+
+const showData = function () {
+  const sql = `
+    SELECT 
+    employees.first_name,
+    employees.last_name,
+    roles.title,
+    roles.salary,
+    departments.name AS department
+    FROM employees
+    LEFT JOIN roles
+    ON employees.role_id = roles.id
+    LEFT JOIN departments
+    ON roles.department_id = departments.id
+    `;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    console.log("\nCompany Detailed Information");
+    console.table(rows);
+  });
+};
+
+module.exports = { showData };
